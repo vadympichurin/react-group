74540ffb5b2093daa302833c6a9115b74b486b78
VadymPic <vadym@mongrov.com>
2019-10-09 11:55:31 +0300

    011- redux_basics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { createStore } from "redux";
 
-import App from "./components/App";
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Route component={App} />
+//   </BrowserRouter>,
+//   document.querySelector("#root")
+// );
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Route component={App} />
-  </BrowserRouter>,
-  document.querySelector("#root")
-);
+const ACTION_TYPES = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+  RESET: "RESET",
+}
+
+const initialCounterState = 100;
+
+console.dir(createStore);
+
+const reducer = (state = initialCounterState, action) => {
+  console.log("Form inside reducer", action);
+
+  switch (action.type) {
+    case ACTION_TYPES.INCREMENT :
+      return state + action.payload;
+    case ACTION_TYPES.DECREMENT :
+      return state - action.payload;
+    case ACTION_TYPES.RESET :
+      return initialCounterState;
+    default:
+      return state;
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+
+
+const incrementAction = {
+  type: ACTION_TYPES.INCREMENT,
+  payload: 5,
+};
+
+const decrementAction = {
+  type: ACTION_TYPES.DECREMENT,
+  payload: 10,
+};
+
+const reset = {
+  type: ACTION_TYPES.RESET,
+};
+
+store.dispatch(incrementAction);
+console.log(store.getState());
+
+store.dispatch(incrementAction);
+console.log(store.getState());
+
+store.dispatch(incrementAction);
+console.log(store.getState());
+
+store.dispatch(decrementAction);
+console.log(store.getState());
+
+store.dispatch(reset);
+console.log(store.getState());
